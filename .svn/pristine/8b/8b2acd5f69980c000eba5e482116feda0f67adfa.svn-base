fbc2b3c9d230c3d38953a39c8903e80410736a3a
qinglipan <pan531945730@126.com>
2018-03-08 17:29:19 +0800

    vue.js

---
+++ .svn/pristine/8b/8b2acd5f69980c000eba5e482116feda0f67adfa.svn-base
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+import load from './load'
+import wxPay from './wxPay'
+
+
+
+export default new Vuex.Store({
+    modules: {
+        load,
+        wxPay
+    },
+})
---
+++ src/store/vux.js
@@ -0,0 +1,29 @@
+/*
+ *  文件说明
+ *  日期：2017/7/17.
+ *  作者：Math
+ * */
+
+export default {
+    state: {
+        pageScrollTop: 0,
+        isLoading: false,
+        direction: 'forward'
+    },
+    mutations: {
+        updateScrollTop (state, payload) {
+            state.pageScrollTop = payload.top
+        },
+        updateLoadingStatus (state, payload) {
+            state.isLoading = payload.isLoading
+        },
+        updateDirection (state, payload) {
+            state.direction = payload.direction
+        }
+    },
+    actions: {
+        updateScrollTop ({commit}, top) {
+            commit({type: 'updateScrollTop', top: top})
+        }
+    }
+}
